da873a668470320771a3c7196bc61dfa3f82916e
Zascu <zascu.github@gmail.com>
2023-03-11 17:53:24 +0200

    Create three themes for the page

---
+++ src/js/data/themes.js
@@ -0,0 +1,21 @@
+const THEMES = [
+  {
+    accentColor: 'var(--clr-red)',
+    accentDarkColor: 'var(--clr-red-dark)',
+    image: 'url(\'/public/img/nike-shoe-1.png\')'
+  },
+
+  {
+    accentColor: 'var(--clr-blue)',
+    accentDarkColor: 'var(--clr-blue-dark)',
+    image: 'url(\'/public/img/nike-shoe-3.png\')'
+  },
+  
+  {
+    accentColor: 'var(--clr-purple)',
+    accentDarkColor: 'var(--clr-purple-dark)',
+    image: 'url(\'/public/img/nike-shoe-2.png\')'
+  }
+];
+
+export default THEMES;
